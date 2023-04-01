644427bc594f9b799e3367bc3d0c70de95201344
Andreu Montagut Osuna <andmonosu@alum.us.es>
2023-04-01 19:40:17 +0200

    Added days titles, month title and colo by session state

--- app/src/main/java/com/andmonosu/erunning/views/SessionsFragment.kt
+++ app/src/main/java/com/andmonosu/erunning/views/SessionsFragment.kt
@@ -5,19 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.view.children
 import com.andmonosu.erunning.R
+import com.andmonosu.erunning.SessionObjectiveState
 import com.andmonosu.erunning.databinding.FragmentSessionsBinding
 import com.kizitonwose.calendar.core.CalendarDay
-import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
+import com.kizitonwose.calendar.core.CalendarMonth
+import com.kizitonwose.calendar.core.DayPosition
+import com.kizitonwose.calendar.core.daysOfWeek
 import com.kizitonwose.calendar.view.CalendarView
 import com.kizitonwose.calendar.view.MonthDayBinder
+import com.kizitonwose.calendar.view.MonthHeaderFooterBinder
+import com.kizitonwose.calendar.view.ViewContainer
+import java.time.DayOfWeek
 import java.time.YearMonth
+import java.time.format.TextStyle
+import java.util.Locale
 
 class SessionsFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
     private lateinit var binding: FragmentSessionsBinding
+    private lateinit var monthTv:TextView
+    private val currentMonth = YearMonth.now()
     private val monthCalendarView: CalendarView get() = binding.calendarView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,27 +46,90 @@ class SessionsFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_sessions, container, false)
         binding = FragmentSessionsBinding.bind(view)
-        initCalendar()
+        initComponents(view)
+        initCalendar(view)
         return view
     }
 
-    private fun initCalendar() {
-        monthCalendarView.dayBinder = object : MonthDayBinder<DayViewContainer> {
-            // Called only when a new container is needed.
-            override fun create(view: View) = DayViewContainer(view)
+    private fun initComponents(view: View?) {
+        if (view != null) {
+            monthTv = view.findViewById(R.id.monthTv)
+        }
+    }
 
-            // Called every time we need to reuse a container.
+    private fun initCalendar(view:View) {
+
+        monthCalendarView.dayBinder = object : MonthDayBinder<DayViewContainer> {
+            override fun create(view: View) = DayViewContainer(view)
             override fun bind(container: DayViewContainer, data: CalendarDay) {
-                container.textView.text = data.date.dayOfMonth.toString()
+                val day = data
+                val textView = container.textView
+                textView.text = day.date.dayOfMonth.toString()
+                if (day.position == DayPosition.MonthDate) {
+                    val state = SessionObjectiveState.valueOf("SUCCESS")
+
+                    when (state) {
+                        SessionObjectiveState.SUCCESS -> {
+                            textView.setTextColor(resources.getColor(R.color.white))
+                            textView.setBackgroundResource(R.drawable.session_succed_background)
+                        }
+                        SessionObjectiveState.NOT_SUCCESS -> {
+                            textView.setTextColor(resources.getColor(R.color.white))
+                            textView.setBackgroundResource(R.drawable.session_not_succed_background)
+                        }
+                        SessionObjectiveState.NOT_TRAINED -> {
+                            textView.setTextColor(resources.getColor(R.color.white))
+                            textView.setBackgroundResource(R.drawable.session_not_trained_background)
+                        }
+                    }
+                } else {
+                    textView.visibility = View.INVISIBLE
+                }
             }
         }
-
-        val currentMonth = YearMonth.now()
-        val startMonth = currentMonth.minusMonths(100)  // Adjust as needed
-        val endMonth = currentMonth.plusMonths(100)  // Adjust as needed
-        val firstDayOfWeek = firstDayOfWeekFromLocale() // Available from the library
-        monthCalendarView.setup(startMonth, endMonth, firstDayOfWeek)
+        val startMonth = currentMonth.minusMonths(100)
+        val endMonth = currentMonth.plusMonths(100)
+        val daysOfWeek = daysOfWeek(firstDayOfWeek = DayOfWeek.MONDAY)
+        initCalendarTitles(view,daysOfWeek)
+        initMonth(currentMonth)
+        monthCalendarView.setup(startMonth, endMonth, daysOfWeek.first())
         monthCalendarView.scrollToMonth(currentMonth)
+        monthCalendarView.monthScrollListener = { updateMonth()}
+    }
+
+    private fun updateMonth() {
+        val month = monthCalendarView.findFirstVisibleMonth()?.yearMonth
+        if (month != null) {
+            monthTv.text = month.month.getDisplayName(TextStyle.FULL,Locale.ENGLISH)
+        }
+    }
+
+    private fun initMonth(currentMonth: YearMonth) {
+        monthTv.text = currentMonth.month.getDisplayName(TextStyle.FULL,Locale.ENGLISH)
+    }
+
+    private fun initCalendarTitles(view: View, daysOfWeek: List<DayOfWeek>) {
+        class MonthViewContainer(view: View) : ViewContainer(view) {
+            val titlesContainer = view as ViewGroup
+        }
+
+
+
+        monthCalendarView.monthHeaderBinder = object : MonthHeaderFooterBinder<MonthViewContainer> {
+            override fun create(view: View) = MonthViewContainer(view)
+            override fun bind(container: MonthViewContainer, data: CalendarMonth) {
+                if (container.titlesContainer.tag == null) {
+                    container.titlesContainer.tag = data.yearMonth
+
+                    container.titlesContainer.children.map { it as TextView }
+                        .forEachIndexed { index, textView ->
+                            val dayOfWeek = daysOfWeek[index]
+                            val title = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault())
+                            textView.text = title
+                        }
+                }
+            }
+        }
     }
 
     companion object {
