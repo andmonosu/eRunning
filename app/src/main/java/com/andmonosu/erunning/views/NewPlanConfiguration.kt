79f18dc410f43cc72595cbcf75409d006e6f6214
Andreu Montagut Osuna <andmonosu@alum.us.es>
2023-06-08 19:35:20 +0200

    New files added

---
+++ app/src/main/java/com/andmonosu/erunning/views/NewPlanConfiguration.kt
@@ -0,0 +1,13 @@
+package com.andmonosu.erunning.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.andmonosu.erunning.R
+
+class NewPlanConfiguration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_plan_configuration)
+
+    }
+}
